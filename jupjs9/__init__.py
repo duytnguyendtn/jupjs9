9457ffa2cbaa65560912c75d24bdd91f46c9f973
Abdu Zoghbi <a.zoghbi@nasa.gov>
2022-10-18 11:48:32 -0400

    added jupyter_server_proxy stuff for js9; to be used instead of jupyter_notebook_config.py

--- jupjs9/__init__.py
+++ jupjs9/__init__.py
@@ -2,7 +2,27 @@ __version__ = '0.0.1'
 
 from .app import JupJs9App
 
-appentry = launch_new_instance = JupJs9App.launch_instance
+# Main js9 server to serve the html page
+# initialized from pyproject.toml
+def js9_main_server():
+    return {
+        'command': ['python', '-m', 'http.server', '-d', '/opt/js9-web', '{port}'],
+        'launcher_entry': {
+            'enabled': True,
+            'title': 'JS9'
+        }}
+
+# Server side helpers to support external communication with JS9 
+# (via the shell and Python) and handle the display of large files.
+# See: https://js9.si.edu/js9/help/helper.html
+# initlizaed from pyproject.toml
+def js9_helper_server():
+    return {
+        'command': ['bash', '-c', 'node /opt/js9-web/js9Helper.js'],
+        'port': 2718,
+        'launcher_entry': {
+           'enabled': False,
+        }}
     
 
 def _jupyter_server_extension_points():
